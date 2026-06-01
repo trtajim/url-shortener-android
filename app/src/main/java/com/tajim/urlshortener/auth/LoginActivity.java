be6b9b4be882be0c18822c4327dd5e90c99c712b
Tajim <com.tajim@gmail.com>
2026-06-01 12:31:08 +0600

    feat(auth): create Login/Register activities and connect LandingActivity buttons to navigation

--- app/src/main/java/com/tajim/urlshortener/auth/LoginActivity.java
+++ app/src/main/java/com/tajim/urlshortener/auth/LoginActivity.java
@@ -1,50 +1,36 @@
-package com.tajim.urlshortener.ui;
+package com.tajim.urlshortener.auth;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.tajim.urlshortener.R;
-import com.tajim.urlshortener.auth.LoginActivity;
-import com.tajim.urlshortener.auth.RegisterActivity;
 import com.tajim.urlshortener.databinding.ActivityLandingBinding;
+import com.tajim.urlshortener.databinding.ActivityLoginBinding;
 
-public class LandingActivity extends AppCompatActivity {
-    ActivityLandingBinding binding;
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         enableEdgeToEdge();
         setupLayout();
-        initClickListeners();
-
-
     }
     private void enableEdgeToEdge(){
         EdgeToEdge.enable(this);
     }
     private void setupLayout(){
-        binding = ActivityLandingBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    private void initClickListeners(){
-        binding.btnLogin.setOnClickListener(v->{
-            startActivity(new Intent(LandingActivity.this, LoginActivity.class));
-        });
-        binding.btnRegister.setOnClickListener(v->{
-            startActivity(new Intent(LandingActivity.this, RegisterActivity.class));
-        });
-
     }
 }
