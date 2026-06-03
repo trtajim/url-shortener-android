252f7f6f618acd11f15bce3ce527257377874e31
Tajim <com.tajim@gmail.com>
2026-06-03 14:41:12 +0600

    chore(config): replace hardcoded API URL with BuildConfig field per build type

--- app/src/main/java/com/tajim/urlshortener/api/ApiConfig.java
+++ app/src/main/java/com/tajim/urlshortener/api/ApiConfig.java
@@ -1,7 +1,9 @@
 package com.tajim.urlshortener.api;
 
+import com.tajim.urlshortener.BuildConfig;
+
 public class ApiConfig {
-    public static final String SERVER_BASE_URL = "http://192.168.1.6:8000";
+    public static final String SERVER_BASE_URL = BuildConfig.SERVER_BASE_URL;
 
     public static final String API_BASE_URL = SERVER_BASE_URL+"/api";
 }
